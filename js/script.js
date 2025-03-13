12254a7d4020afbc9b0cdf218058e8f1557f3ffd
ZZBaron <zafrensilli@gmail.com>
2025-03-12 22:28:22 -0400

    updated website content

--- js/script.js
+++ js/script.js
@@ -14,12 +14,3 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled');
     }
 });
-
-// Form handling
-const form = document.querySelector('.contact-form');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Here you would typically send the form data to a server
-    alert('Message sent! (This is just a demo)');
-    form.reset();
-});
